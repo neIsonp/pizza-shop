7f464eda55b503afc69c2d3c3bdfd00de9367ce0
neIsonp <nelsonponte37@gmail.com>
2025-01-23 10:30:23 -0100

    create pagination componente

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@/components/ui/button";
+import { TableRow, TableCell } from "@/components/ui/table";
+import { Search, ArrowRight, X } from "lucide-react";
+
+export default function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant={"outline"} size="xs">
+          <Search className="h-3 w-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono">oiwquher82347fh23ribf</TableCell>
+      <TableCell className="text-muted-foreground">Há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Nelson Ponte</TableCell>
+      <TableCell className="font-medium">70,03€</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 h-3 w-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  );
+}
